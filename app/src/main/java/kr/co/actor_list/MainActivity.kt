1bbb4ee1606694d6432ea5b46a88275edba1bb26
JISOO2206 <900522z@gmail.com>
2023-03-01 23:49:38 +0900

    만드는 순서 주석으로 기록

--- app/src/main/java/kr/co/actor_list/MainActivity.kt
+++ app/src/main/java/kr/co/actor_list/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //1. nav 만들기
+
+        //2. fragment 만들고 nav에 연결하기
+
+        //3. 리사이클러뷰 만들기
     }
 }
